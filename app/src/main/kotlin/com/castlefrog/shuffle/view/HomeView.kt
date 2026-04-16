9459f5fae0127f85d16d6a03985f828017c0aa23
Paul Lewis-Parker <paularthurlewis@gmail.com>
2026-04-16 12:14:00 -0700

    added hamburger and edit icons to top navigation

--- app/src/main/kotlin/com/castlefrog/shuffle/view/HomeView.kt
+++ app/src/main/kotlin/com/castlefrog/shuffle/view/HomeView.kt
@@ -4,30 +4,43 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(
-    content: @Composable (PaddingValues) -> Unit
+    onMenuClick: () -> Unit = {},
+    onEditClick: () -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit,
 ) {
     Scaffold(
         topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(16.dp)),
-            ) {
-            }
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = onMenuClick) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = onEditClick) {
+                        Icon(Icons.Filled.Edit, contentDescription = "Edit")
+                    }
+                },
+            )
         },
     ) { innerPadding ->
         content(innerPadding)
