8a70c11b732fdcd20e3d74cce1f1d0bd8c5975ff
Paul Lewis-Parker <paularthurlewis@gmail.com>
2026-06-28 09:08:34 -0700

    Update target sdk to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.castlefrog.shuffle"
         minSdk = 36
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
